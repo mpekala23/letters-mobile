b7a0fd2a1a03565e80f3653460a46ef69ef838f8
mpekala23 <mpekala@college.harvard.edu>
2020-05-19 16:09:10 -0400

    al but navigation

---
+++ src/api/index.tsx
@@ -0,0 +1,5 @@
+import url from "url";
+
+export const API_URL = "http://localhost::somethinglater";
+
+url.resolve(API_URL, "fill later");
--- src/components/Statusbar/Statusbar.react.tsx
+++ src/components/Statusbar/Statusbar.react.tsx
@@ -1,11 +1,11 @@
-import * as React from 'react';
-import { View, StatusBar } from 'react-native';
-import { STATUS_BAR_HEIGHT } from 'utils';
+import React from "react";
+import { View, StatusBar } from "react-native";
+import { STATUS_BAR_HEIGHT } from "utils";
 
 /**
 A component to go at the top of the screen behind native status bar on different devices.
 */
-export default function Statusbar() {
+const Statusbar: React.FC = () => {
   return (
     <View style={{ height: STATUS_BAR_HEIGHT, zIndex: 999 }}>
       <StatusBar
@@ -17,4 +17,6 @@ export default function Statusbar() {
       />
     </View>
   );
-}
+};
+
+export default Statusbar;
