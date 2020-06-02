412d08b9a5aff0f8029c190f8b4d69b612ff0ac1
chenxiuqi <xiuqichen8@gmail.com>
2020-06-02 12:37:47 -0400

    [1/x][prettier] formatting + newlines

--- src/views/ReferFriends/ReferFriends.react.tsx
+++ src/views/ReferFriends/ReferFriends.react.tsx
@@ -10,8 +10,8 @@ import { Typography } from "@styles";
 
 class ReferFriendsScreen extends React.Component<Props, State> {
   render() {
-  	return (
-  		<KeyboardAvoidingView
+    return (
+      <KeyboardAvoidingView
         style={Styles.trueBackground}
         behavior="padding"
         enabled
@@ -22,41 +22,41 @@ class ReferFriendsScreen extends React.Component<Props, State> {
             justifyContent: "center",
           }}
         >
-        	{/* TO-DO: Add in image asset when finalized */}
-        	<Text style={[Typography.REGULAR, { marginBottom: 24, textAlign: "center" }]}>
-        		Estimated delivery date: 
+          {/* TO-DO: Add in image asset when finalized */}
+          <Text style={[Typography.REGULAR, { marginBottom: 24, textAlign: "center" }]}>
+            Estimated delivery date:
         		<Text style={[Typography.FONT_BOLD]}> DATE</Text>
+          </Text>
+          <Text style={[Typography.REGULAR, { marginBottom: 36, textAlign: "center" }]}>
+            Your letter to NAME is on the way! Thanks for trusting us to deliver your messages.
         	</Text>
-        	<Text style={[Typography.REGULAR, { marginBottom: 36, textAlign: "center" }]}>
-        		Your letter to NAME is on the way! Thanks for trusting us to deliver your messages.
-        	</Text>
-        	<Text style={[Typography.FONT_BOLD, { marginBottom: 24, textAlign: "center" }]}>
-        		Do you know anyone that could benefit from Ameelio's free services?
+          <Text style={[Typography.FONT_BOLD, { marginBottom: 24, textAlign: "center" }]}>
+            Do you know anyone that could benefit from Ameelio's free services?
         	</Text>
           <View
-            style={{ flexDirection: "row", justifyContent: "space-between",  }}
+            style={{ flexDirection: "row", justifyContent: "space-between", }}
           >
-  					<Button
-  					  buttonText="Skip"
-  					  reverse
-  					  containerStyle={{ width: 80 }}
-  					  textStyle={{fontSize: 16 }}
-  					  onPress={() => {
-  					    console.log("pressed skip button");
-  					  }}
-  					/>
-  					<Button
-  					  buttonText="Share"
-  					  containerStyle={{ width: 80 }}
-  					  textStyle={{fontSize: 16 }}
-  					  onPress={() => {
-  					    console.log("pressed share button");
-  					  }}
-  			    />
-  			  </View>
+            <Button
+              buttonText="Skip"
+              reverse
+              containerStyle={{ width: 80 }}
+              textStyle={{ fontSize: 16 }}
+              onPress={() => {
+                console.log("pressed skip button");
+              }}
+            />
+            <Button
+              buttonText="Share"
+              containerStyle={{ width: 80 }}
+              textStyle={{ fontSize: 16 }}
+              onPress={() => {
+                console.log("pressed share button");
+              }}
+            />
+          </View>
         </View>
       </KeyboardAvoidingView>
-  	);
+    );
   }
 }
 
--- src/views/ReferFriends/ReferFriends.style.tsx
+++ src/views/ReferFriends/ReferFriends.style.tsx
@@ -2,13 +2,13 @@ import { StyleSheet } from "react-native";
 import { Colors } from "@styles";
 
 export default StyleSheet.create({
-  trueBackground: {
-    backgroundColor: "white",
-    flex: 1,
-    flexDirection: "column",
-    padding: 24,
-  },
-  fullWidth: {
-    width: "100%",
-  },
+	trueBackground: {
+		backgroundColor: "white",
+		flex: 1,
+		flexDirection: "column",
+		padding: 24,
+	},
+	fullWidth: {
+		width: "100%",
+	},
 });
