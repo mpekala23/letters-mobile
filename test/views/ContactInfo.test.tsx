a521a19c479f3765b189cc62938f993feddc60df
mpekala23 <mpekala@college.harvard.edu>
2020-06-03 16:08:14 -0400

    ContactInfo tests

--- test/views/ContactInfo.test.tsx
+++ test/views/ContactInfo.test.tsx
@@ -1,18 +1,13 @@
 import * as React from "react";
-import { SplashScreen } from "@views";
+import { ContactInfo } from "@views";
 import { render, toJSON } from "@testing-library/react-native";
 
 const AmeelioLogo = require("@assets/Ameelio_Logo.png");
 
 describe("Splash screen", () => {
   it("should match snapshot", () => {
-    const { container } = render(<SplashScreen />);
+    const { container } = render(<ContactInfo />);
     const tree = toJSON(container);
     expect(tree).toMatchSnapshot();
-  });
-
-  it("should show the logo", () => {
-    const { getByLabelText } = render(<SplashScreen />);
-    expect(getByLabelText("AmeelioLogo")).toBeDefined();
   });
 });
