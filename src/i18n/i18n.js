dc23a050ed5e00b0b83cf5f8a98a3c5a3c46f27a
chenxiuqi <xiuqichen8@gmail.com>
2020-06-08 12:38:31 -0400

    [2/x] get user's locale upon loading app

--- src/i18n/i18n.js
+++ src/i18n/i18n.js
@@ -4,7 +4,8 @@ import en from './locales/en.json';
 import es from './locales/es.json';
 
 i18n.defaultLocale = 'en';
-i18n.locale = 'en';
+// uncomment line below for quick dev testing
+// i18n.locale = 'es';
 i18n.fallbacks = true;
 i18n.translations = { en, es };
 
--- src/views/Splash/Splash.react.tsx
+++ src/views/Splash/Splash.react.tsx
@@ -1,8 +1,14 @@
-import React from "react";
-import { Image, View, StyleSheet } from "react-native";
+import React, { useEffect }from "react";
+import { Image, View, StyleSheet, NativeModules, Platform } from "react-native";
 const AmeelioLogo = require("@assets/Ameelio_Logo.png");
+import i18n from 'i18n-js';
 
 const SplashScreen: React.FC = () => {
+  useEffect(() => {
+    const locale = getDeviceLocale();
+    i18n.locale = locale;
+  });
+
   return (
     <View
       style={{
@@ -20,5 +26,13 @@ const SplashScreen: React.FC = () => {
     </View>
   );
 };
+
+const getDeviceLocale = () => {
+  // returns locale in format of "en_US" (eng.) or "es_US" (sp.)
+  const locale = Platform.OS === 'ios'
+    ? NativeModules.SettingsManager.settings.AppleLocale
+    : NativeModules.I18nManager.localeIdentifier;
+  return locale.substring(0, 2);
+}
 
 export default SplashScreen;
