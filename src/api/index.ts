99a989969863090dafd66205f8299373e03193fb
mpekala23 <mpekala@college.harvard.edu>
2020-06-16 18:47:19 -0400

    full project initial

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,12 +1,12 @@
-import store from "@store";
-import { Linking } from "react-native";
-import { loginUser, logoutUser } from "@store/User/UserActions";
-import { User, UserCredentials, UserInfo } from "@store/User/UserTypes";
-import url from "url";
+import store from '@store';
+import { Linking } from 'react-native';
+import { loginUser, logoutUser } from '@store/User/UserActions';
+import { User, UserCredentials, UserInfo } from '@store/User/UserTypes';
+import url from 'url';
 
-const MOCK_API_IP = process.env.MOCK_API_IP;
+const { MOCK_API_IP } = process.env;
 
-export const API_URL = "http://" + MOCK_API_IP + ":9000/api/";
+export const API_URL = `http://${MOCK_API_IP}:9000/api/`;
 
 export interface UserResponse {
   type: string;
@@ -14,42 +14,42 @@ export interface UserResponse {
 }
 
 export function fetchTimeout<T>(
-  url: string,
-  options: object,
+  fetchUrl: string,
+  options: Record<string, unknown>,
   timeout = 3000
 ): Promise<Response | T> {
   return Promise.race([
-    fetch(url, options),
+    fetch(fetchUrl, options),
     new Promise<Response | T>((_, reject) =>
-      setTimeout(() => reject(new Error("timeout")), timeout)
+      setTimeout(() => reject(new Error('timeout')), timeout)
     ),
   ]);
 }
 
 /** Dummy function atm, once I implement persistent storage I will replace. */
-export async function loadToken() {
+export function loadToken(): void {
   const dummyData: User = {
-    id: "6",
-    firstName: "Team",
-    lastName: "Ameelio",
-    email: "team@ameelio.org",
-    cell: "4324324432",
-    address1: "Somewhere",
-    country: "USA",
-    zipcode: "12345",
-    city: "New Haven",
-    state: "CT",
+    id: '6',
+    firstName: 'Team',
+    lastName: 'Ameelio',
+    email: 'team@ameelio.org',
+    phone: '4324324432',
+    address1: 'Somewhere',
+    country: 'USA',
+    postal: '12345',
+    city: 'New Haven',
+    state: 'CT',
   };
   store.dispatch(loginUser(dummyData));
 }
 
 /** Dummy function atm, once I implement mock login API calls (and then real calls) I will replace */
-export async function login(cred: UserCredentials) {
-  const response = await fetchTimeout<Response>(url.resolve(API_URL, "login"), {
-    method: "POST",
+export async function login(cred: UserCredentials): Promise<User> {
+  const response = await fetchTimeout<Response>(url.resolve(API_URL, 'login'), {
+    method: 'POST',
     headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify({
       email: cred.email,
@@ -57,7 +57,7 @@ export async function login(cred: UserCredentials) {
     }),
   });
   const body = await response.json();
-  if (body.type == "error") {
+  if (body.type === 'error') {
     throw Error(body.data);
   }
   const userData: User = {
@@ -65,11 +65,11 @@ export async function login(cred: UserCredentials) {
     firstName: body.data.firstName,
     lastName: body.data.lastName,
     email: body.data.email,
-    cell: body.data.cell,
+    phone: body.data.cell,
     address1: body.data.address1,
     address2: body.data.address2 || null,
     country: body.data.country,
-    zipcode: body.data.zipCode,
+    postal: body.data.zipCode,
     city: body.data.city,
     state: body.data.state,
   };
@@ -77,20 +77,20 @@ export async function login(cred: UserCredentials) {
   return userData;
 }
 
-export async function register(data: UserInfo) {
+export async function register(data: UserInfo): Promise<User> {
   const response = await fetchTimeout<Response>(
-    url.resolve(API_URL, "register"),
+    url.resolve(API_URL, 'register'),
     {
-      method: "POST",
+      method: 'POST',
       headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify(data),
     }
   );
   const body = await response.json();
-  if (body.type == "error") {
+  if (body.type === 'error') {
     throw Error(body.data);
   }
   const userData: User = {
@@ -98,11 +98,11 @@ export async function register(data: UserInfo) {
     firstName: body.data.firstName,
     lastName: body.data.lastName,
     email: body.data.email,
-    cell: body.data.cell,
+    phone: body.data.cell,
     address1: body.data.address1,
     address2: body.data.address2 || null,
     country: body.data.country,
-    zipcode: body.data.zipcode,
+    postal: body.data.zipcode,
     city: body.data.city,
     state: body.data.state,
   };
@@ -110,13 +110,11 @@ export async function register(data: UserInfo) {
   return userData;
 }
 
-export async function getFacilities(text: string) {}
-
-export async function facebookShare(shareUrl: string) {
+export async function facebookShare(shareUrl: string): Promise<void> {
   const supportedUrl = await Linking.canOpenURL(shareUrl);
   if (supportedUrl) {
     await Linking.openURL(shareUrl);
   } else {
-    throw Error("Share Url not supported");
+    throw Error('Share Url not supported');
   }
 }
