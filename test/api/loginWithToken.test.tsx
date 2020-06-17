63c6d146fc634c9df4137207051ff15bc183e2ea
mpekala23 <mpekala@college.harvard.edu>
2020-06-17 13:50:00 -0400

    fix test whoops

--- test/api/loginWithToken.test.tsx
+++ test/api/loginWithToken.test.tsx
@@ -47,22 +47,19 @@ describe("api loginWithToken", () => {
       `*`,
       JSON.stringify({
         status: "SUCCESS",
-        message: "Invalid token",
-        data: [
-          {
-            id: "6",
-            first_name: "Team",
-            last_name: "Ameelio",
-            email: "team@ameelio.org",
-            phone: "4324324432",
-            addr_line_1: "Somewhere",
-            country: "USA",
-            postal: "12345",
-            city: "New Haven",
-            state: "CT",
-            token: "dummy token",
-          },
-        ],
+        data: {
+          id: "6",
+          first_name: "Team",
+          last_name: "Ameelio",
+          email: "team@ameelio.org",
+          phone: "4324324432",
+          addr_line_1: "Somewhere",
+          country: "USA",
+          postal: "12345",
+          city: "New Haven",
+          state: "CT",
+          token: "dummy token",
+        },
       }),
       { overwriteRoutes: false }
     );
