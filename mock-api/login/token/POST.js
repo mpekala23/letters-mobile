c0c02298d1840fe36aee71fff6fd4cb95df95ca1
mpekala23 <mpekala@college.harvard.edu>
2020-06-22 14:26:12 -0400

    prettier

--- mock-api/login/token/POST.js
+++ mock-api/login/token/POST.js
@@ -1,28 +1,28 @@
 module.exports = (req, res) => {
   const { token } = req.body;
 
-  if (token !== "dummy token") {
+  if (token !== 'dummy token') {
     return res.status(401).send({
-      status: "ERROR",
-      message: "Invalid token",
+      status: 'ERROR',
+      message: 'Invalid token',
       data: [],
     });
   }
 
   return res.status(201).send({
-    status: "OK",
+    status: 'OK',
     data: {
-      id: "6",
-      first_name: "Team",
-      last_name: "Ameelio",
-      email: "team@ameelio.org",
-      phone: "4324324432",
-      addr_line_1: "Somewhere",
-      country: "USA",
-      postal: "12345",
-      city: "New Haven",
-      state: "CT",
-      token: "dummy token",
+      id: '6',
+      first_name: 'Team',
+      last_name: 'Ameelio',
+      email: 'team@ameelio.org',
+      phone: '4324324432',
+      addr_line_1: 'Somewhere',
+      country: 'USA',
+      postal: '12345',
+      city: 'New Haven',
+      state: 'CT',
+      token: 'dummy token',
     },
   });
 };
