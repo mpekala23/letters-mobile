b3d4847b9188c95055ab7c0934b6238ff0fa2a54
mpekala23 <mpekala@college.harvard.edu>
2020-09-18 14:27:28 -0400

    acutally fix wrong date bug

--- src/utils/Notifications.ts
+++ src/utils/Notifications.ts
@@ -3,16 +3,17 @@ import { NotifTypes } from '@store/Notif/NotifTypes';
 import { deleteDraft } from '@api/User';
 import i18n from '@i18n';
 import { Contact } from 'types';
-import { format } from 'date-fns';
+import { addDays, format } from 'date-fns';
 
 export function handleNotificationsAfterSend(activeContact: Contact): void {
   Notifs.cancelAllNotificationsByType(NotifTypes.NoFirstLetter);
   Notifs.cancelAllNotificationsByType(NotifTypes.Drought);
   Notifs.scheduleNotificationInDays(
     {
-      title: `${i18n.t('Notifs.happy')} ${format(new Date(), 'dddd')}! ${i18n.t(
-        'Notifs.readyToSendAnother'
-      )} ${activeContact.firstName}?`,
+      title: `${i18n.t('Notifs.happy')} ${format(
+        addDays(new Date(), 7),
+        'dddd'
+      )}! ${i18n.t('Notifs.readyToSendAnother')} ${activeContact.firstName}?`,
       body: `${i18n.t('Notifs.clickHereToBegin')}`,
       type: NotifTypes.Drought,
       data: {
