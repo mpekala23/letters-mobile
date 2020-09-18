3de7d7667524dc1869ce833db68cfbb0299cd355
mpekala23 <mpekala@college.harvard.edu>
2020-09-18 14:20:14 -0400

    fix last bug

--- src/utils/Notifications.ts
+++ src/utils/Notifications.ts
@@ -3,13 +3,14 @@ import { NotifTypes } from '@store/Notif/NotifTypes';
 import { deleteDraft } from '@api/User';
 import i18n from '@i18n';
 import { Contact } from 'types';
+import { format } from 'date-fns';
 
 export function handleNotificationsAfterSend(activeContact: Contact): void {
   Notifs.cancelAllNotificationsByType(NotifTypes.NoFirstLetter);
   Notifs.cancelAllNotificationsByType(NotifTypes.Drought);
   Notifs.scheduleNotificationInDays(
     {
-      title: `${i18n.t('Notifs.happy')} ${new Date().toDateString()}! ${i18n.t(
+      title: `${i18n.t('Notifs.happy')} ${format(new Date(), 'dddd')}! ${i18n.t(
         'Notifs.readyToSendAnother'
       )} ${activeContact.firstName}?`,
       body: `${i18n.t('Notifs.clickHereToBegin')}`,
