0be34f75ec7202dfe00e5f63f0dfad09525e0305
mpekala23 <mpekala@college.harvard.edu>
2020-09-24 01:15:09 -0400

    Update to pngs, cleanup for submit

--- src/assets/stickers/index.tsx
+++ src/assets/stickers/index.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { Image as ImageComponent } from 'react-native';
-
-import { AsyncImage } from '@components';
 import { Sticker } from 'types';
 
 import Ameelio from './Ameelio.png';
@@ -11,10 +9,8 @@ import HappyBirthday from './HappyBirthday.png';
 import Hearts from './Hearts.png';
 import LoveYou from './LoveYou.png';
 import MissYou from './MissYou.png';
-import Peas from './Peas.png';
 import Stars from './Stars.png';
 import Sunshine from './Sunshine.png';
-import TrashCan from './TrashCan.png';
 import TwoPeas from './TwoPeas.png';
 
 /*
@@ -72,42 +68,92 @@ const STICKERS_SVG: Sticker[] = [
   },
 ]; */
 
+const AmeelioComponent = (
+  <ImageComponent source={Ameelio} style={{ width: '100%', height: '100%' }} />
+);
+
 const BestDadComponent = (
   <ImageComponent source={BestDad} style={{ width: '100%', height: '100%' }} />
 );
 
+const EggcelentComponent = (
+  <ImageComponent
+    source={Eggcelent}
+    style={{ width: '100%', height: '100%' }}
+  />
+);
+
+const HappyBirthdayComponent = (
+  <ImageComponent
+    source={HappyBirthday}
+    style={{ width: '100%', height: '100%' }}
+  />
+);
+
+const HeartsComponent = (
+  <ImageComponent source={Hearts} style={{ width: '100%', height: '100%' }} />
+);
+
+const LoveYouComponent = (
+  <ImageComponent source={LoveYou} style={{ width: '100%', height: '100%' }} />
+);
+
+const MissYouComponent = (
+  <ImageComponent source={MissYou} style={{ width: '100%', height: '100%' }} />
+);
+
+const StarsComponent = (
+  <ImageComponent source={Stars} style={{ width: '100%', height: '100%' }} />
+);
+
+const SunshineComponent = (
+  <ImageComponent source={Sunshine} style={{ width: '100%', height: '100%' }} />
+);
+
+const TwoPeasComponent = (
+  <ImageComponent source={TwoPeas} style={{ width: '100%', height: '100%' }} />
+);
+
 const STICKERS: Sticker[] = [
   {
-    component: BestDadComponent,
-    name: '#1 Dad',
+    component: AmeelioComponent,
+    name: 'Sent by Ameelio',
   },
   {
     component: BestDadComponent,
     name: '#1 Dad',
   },
   {
-    component: BestDadComponent,
-    name: '#1 Dad',
+    component: EggcelentComponent,
+    name: 'Eggcelent',
   },
   {
-    component: BestDadComponent,
-    name: '#1 Dad',
+    component: HappyBirthdayComponent,
+    name: 'Happy Birthday',
   },
   {
-    component: BestDadComponent,
-    name: '#1 Dad',
+    component: HeartsComponent,
+    name: 'Hearts',
   },
   {
-    component: BestDadComponent,
-    name: '#1 Dad',
+    component: LoveYouComponent,
+    name: 'Love You',
   },
   {
-    component: BestDadComponent,
-    name: '#1 Dad',
+    component: MissYouComponent,
+    name: 'Miss You',
   },
   {
-    component: BestDadComponent,
-    name: '#1 Dad',
+    component: StarsComponent,
+    name: 'Stars',
+  },
+  {
+    component: SunshineComponent,
+    name: 'Sunshine',
+  },
+  {
+    component: TwoPeasComponent,
+    name: 'Two Peas',
   },
 ];
 
