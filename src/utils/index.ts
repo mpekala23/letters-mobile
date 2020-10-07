795b310935ef5d9bc2a5c9b66fe1fea2a8e95749
mpekala23 <mpekala@college.harvard.edu>
2020-10-07 02:21:43 -0400

    remove true

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -248,7 +248,6 @@ export function estimateDelivery(date: Date, status?: MailStatus): Date {
 export const RELEASE_CHANNEL = Constants.manifest.releaseChannel;
 
 export function isProduction(): boolean {
-  return true;
   if (!RELEASE_CHANNEL) return false;
   return RELEASE_CHANNEL.indexOf('prod') !== -1;
 }
