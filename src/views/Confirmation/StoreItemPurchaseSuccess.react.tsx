331d289770535c0cc453326e5923d4ba55ef8ea9
gabrielsaruhashi <gabriel.saruhashi@gmail.com>
2020-10-29 10:05:56 -0300

    redirection works, title does not overflow

--- src/views/Confirmation/StoreItemPurchaseSuccess.react.tsx
+++ src/views/Confirmation/StoreItemPurchaseSuccess.react.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, Text } from 'react-native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { AppStackParamList, Screens } from '@utils/Screens';
@@ -20,6 +20,7 @@ interface Props {
 const StoreItemPurchaseSuccessScreen: React.FC<Props> = ({
   navigation,
 }: Props) => {
+  useEffect(() => console.log('here'));
   return (
     <View style={Styles.trueBackground}>
       <View style={Styles.contentBackground}>
